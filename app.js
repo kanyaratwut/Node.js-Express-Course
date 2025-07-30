30ec21cae91ac60931fe7e2b784d43e0a3959160
champagne <dev@dosetech.co>
2025-07-30 20:26:28 +0700

    add template

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+console.log('Serving static from: ', path.join(__dirname, 'public'));
+
+
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public'))); //เรียกใช้ path ไปหาไฟล์ public
+
 
 app.get('/', (req, res) => {
     res.send('Hello Kanyarat!');
